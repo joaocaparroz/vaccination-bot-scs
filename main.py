9c2890991e212588575b511c98e112ca64f58f1d
João Caparroz <joaocaparroz@gmail.com>
2021-07-23 15:11:28 -0300

    Added filtering and sorting

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     vaccine_list = check_website()
+    vaccine_list = [x for x in vaccine_list if x.get('availability')]
+    vaccine_list = sorted(vaccine_list, key=lambda elem: "%02d %s" % (elem['dose'], elem['age']))
     if check_need_to_send_message(vaccine_list, False):
         send_pushbullet_message(vaccine_list)
         send_telegram_message(vaccine_list)
